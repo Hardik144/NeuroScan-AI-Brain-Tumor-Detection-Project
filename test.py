0740951e0f0ca59d12ff84a8d968a5354971df67
Hardik144 <patidarhardik81@gmail.com>
2025-11-23 16:15:30 +0530

    feat(model): add SELU activation functions and dropout (p=0.4) regularization

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import torch
+from torch.nn import Sequential, Linear, SELU, Dropout, LogSigmoid
 from torchvision.models import resnet50
-# Draft ResNet50 model setup
 model = resnet50(weights=None)
+n_inputs = model.fc.in_features
+model.fc = Sequential(Linear(n_inputs, 2048), SELU(), Dropout(0.4), Linear(2048, 2048), SELU(), Dropout(0.4), Linear(2048, 4), LogSigmoid())
