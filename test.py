30532447b2719d1c5de8cbe222a61275ba034e24
Hardik144 <patidarhardik81@gmail.com>
2025-11-26 11:00:20 +0530

    feat(cli): add command-line argument parser for input image evaluation

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-import torch
-import torch.nn.functional as F
-from torch.nn import Sequential, Linear, SELU, Dropout, LogSigmoid
-from torchvision.models import resnet50
-LABELS = ['None', 'Meningioma', 'Glioma', 'Pitutary']
-def predict_probs(output):
-    probs = torch.exp(output)
-    return probs / probs.sum(dim=1, keepdim=True)
+import argparse, sys
+parser = argparse.ArgumentParser()
+parser.add_argument('--image', required=True)
