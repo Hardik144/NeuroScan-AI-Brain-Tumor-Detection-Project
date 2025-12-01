4e65a70310c4248bda53de12dbced00936f61b03
Hardik144 <patidarhardik81@gmail.com>
2025-12-01 15:45:50 +0530

    feat(api): create /api/predict JSON endpoint for asynchronous inference

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import flask, os
+import flask, os, torch
 app = flask.Flask(__name__, template_folder='templates')
-UPLOAD_FOLDER = os.path.join('static', 'photos')
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    return flask.jsonify({'status': 'online'})
