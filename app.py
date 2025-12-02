6c9b07b3fd39113cff91654ae7d42f657ba73793
Hardik144 <patidarhardik81@gmail.com>
2025-12-02 13:30:20 +0530

    feat(api): implement /api/sample/<filename> route for instant scan loading

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import flask, os, torch
+import flask, os
 app = flask.Flask(__name__, template_folder='templates')
-@app.route('/api/predict', methods=['POST'])
-def api_predict():
-    return flask.jsonify({'status': 'online'})
+@app.route('/api/sample/<filename>')
+def api_sample(filename):
+    return flask.send_from_directory('Brain-Tumor-Test-Images', filename)
